34112d231a04e4bdcfd82222be5207b79196ec66
ditya17 <nitya17nitya@gmail.com>
2024-02-03 17:54:26 +0530

    initial setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className='bg-black text-center'  >
+      <h1 className="text-5xl text-white font-bold underline">
+      Welcome to CUET
+    </h1>
+    </div>
+  );
+}
+
+export default App;
